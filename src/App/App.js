0a30bd24aa4c50da02bbb3ad08df5d2ba7a7450e
dsdunn <dsdunnmusic@gmail.com>
2018-11-17 16:13:04 -0700

    Adjust file structure

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,21 +7,17 @@ class App extends Component {
   }
 
   componentDidMount() {
-    // this.callApi()
-    // console.log('mount')
   }
 
   handleLogin = async () => {
     window.location = 'http://localhost:8888/login';
   }
 
-  //make auth call, redirect to spotify auth, user clicks ok, spotaut redirects to /callback which displays info.
-
   render() {
     return (
       <div className="App">
       hellooooo?
-        <p onClick={this.handleLogin}>login</p>
+        <button onClick={this.handleLogin}>login</button>
       </div>
     );
   }
