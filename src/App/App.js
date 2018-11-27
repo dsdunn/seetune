3a66801695957dd623f6d3651a0c6b3f8737c7c9
dsdunn <dsdunnmusic@gmail.com>
2018-11-27 15:53:53 -0700

    Refactor App

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import { getUser } from '../apiCalls';
+import { getUser, getTopTracks } from '../apiCalls';
 import './App.css';
 
 import Login from '../Login/Login';
@@ -8,22 +8,34 @@ import User from '../User/User';
 
 class App extends Component {
   state = {
-    accesstoken: ''
+    token: ''
   }
 
-  async componentDidMount() {
+  componentDidMount() {
+    let token = window.location.href.split('=')[1] || '';
 
-    let accesstoken = window.location.href.split('=')[1] || '';
-    let user = await getUser(accesstoken);
+    if (token){
+      this.setUser(token);
+    }
+  }
 
-    this.setState({ accesstoken, user });
+  async setUser (token) {
+    let user = await getUser(token);
+
+    this.setState({token, user})
+    this.getTopData(token);
+  }
+
+  async getTopData (token) {
+    let topTracks = await getTopTracks(token);
+    // let topArtists = awain getTopArtists(token);
   }
 
   render() {
     return (
       <div>
         <h1>SeeTune</h1>
-        { !this.state.accesstoken && <Login/> }
+        { !this.state.token && <Login/> }
         {this.state.user && <User user={this.state.user} />}
       </div>
     );
--- src/User/User.js
+++ src/User/User.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 
 const User = ( {user} ) => {
-  return (
-    <div className='user'>
-      <p>user:</p>
-      <h3>{user.display_name}</h3>
-      <img src={user.images[0].url}/>
-    </div>
-    )
+  if(user.display_name) {
+    return (
+      <div className='user'>
+        <p>user:</p>
+        <h3>{user.display_name}</h3>
+        <img src={user.images[0].url}/>
+      </div>
+      )
+  } return null;
 }
 
 export default withRouter(User);
