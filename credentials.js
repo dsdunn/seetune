8a21a279dba063d35e28cef59fa4449203355ee4
dsdunn <dsdunnmusic@gmail.com>
2019-01-01 21:34:15 -0700

    Add credentials ...again?

---
+++ credentials.js
@@ -0,0 +1,6 @@
+
+module.exports = ({
+  SPOTIFY_CLIENT_ID : 'bbe317c2cf884bd98d05b96cebb4276a',
+  SPOTIFY_CLIENT_SECRET : '3524f4972452471abf5d69e8242895d9'
+});
+
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+
 const express = require('express');
 const querystring = require('querystring');
 const request = require('request');
 const cors = require('cors');
 
+const credentials =  require('./credentials.js');
+console.log(credentials.SPOTIFY_CLIENT_ID);
+
 const app = express();
 
 const redirect_uri = process.env.REDIRECT_URI || 'http://localhost:8888/callback';
-const client_id = process.env.SPOTIFY_CLIENT_ID;
-const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+const client_id = process.env.SPOTIFY_CLIENT_ID || credentials.SPOTIFY_CLIENT_ID;
+const client_secret = process.env.SPOTIFY_CLIENT_SECRET || credentials.SPOTIFY_CLIENT_SECRET;
 
 app.use(cors())
 
