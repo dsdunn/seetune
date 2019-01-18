00783830c0f5b5882460266564288e2ff565ecfa
dsdunn <dsdunnmusic@gmail.com>
2019-01-18 12:06:05 -0700

    Add transition to axes

--- src/Visualizations/TempoGraph.js
+++ src/Visualizations/TempoGraph.js
@@ -31,10 +31,6 @@ class TempoGraph extends Component {
   componentDidUpdate(prevProps, prevState) {
 
     if (!prevProps.topTracks && this.props.topTracks) {
-      this.setState({
-        topTracks: this.props.topTracks
-      })
-
       this.drawGraph();
     }               
   }
@@ -46,7 +42,6 @@ class TempoGraph extends Component {
   }
 
   drawGraph = (topTracks = this.props.topTracks, param = this.state.param) => {
-    console.log('drawGraph')
     let sortedTracks = this.sortTracks(topTracks, param);
     let height = this.height;
     let x = d3.scaleBand()
@@ -54,21 +49,19 @@ class TempoGraph extends Component {
       .padding(0.1);
     let y = d3.scaleLinear()
       .range([this.height, 0])
-    
-
-    x.domain(sortedTracks.map(function(d){ return d.title }))
-
-    y.domain([0, d3.max(sortedTracks, function(d) { return d[param] }) + 20])
-
     let graph = this.svgContainer.selectAll('.bar')
       .data(sortedTracks, d => { return d.title; });
 
     let t = d3.transition().duration(1000);
+    
+    let x_axis = this.svgContainer.append('g')
+      .attr('class', 'x axis')
+      .attr('transform', 'translate(0,' + (this.height + 5) + ')')
+    let y_axis = this.svgContainer.append('g')
+      .attr('class', 'y axis');
 
-    graph.exit()
-      .transition(t)
-      .style('opacity', 1e-6)
-      .remove()
+    x.domain(sortedTracks.map(function(d){ return d.title }))
+    y.domain([0, d3.max(sortedTracks, function(d) { return d[param] }) + 20])
 
     graph.enter()
         .append('rect')
@@ -81,50 +74,23 @@ class TempoGraph extends Component {
         .attr('height', function(d) { return height - y(d[param]); })
         .style('fill', 'steelblue')
 
+    d3.select('.x').transition(t)
+        .call(d3.axisBottom().scale(x));
 
-    this.svgContainer.append('g')
-        .attr('transform', 'translate(0,' + (this.height + 5) + ')')
-        .call(d3.axisBottom(x))
-      .selectAll('text')
-        .attr('transform', 'translate(-11,3) rotate(290)')
-        .style('text-anchor', 'end');
-
-    this.svgContainer.append('g')
-        .call(d3.axisLeft(y))
-
+    d3.selectAll('.x text')
+      .attr('transform', 'translate(-11,3) rotate(290)')
+      .style('text-anchor', 'end');
+      
+    d3.select('.y').transition(t)
+        .call(d3.axisLeft().scale(y));
   }
 
   handleParamChange = (event) => {
     let param = event.target.value;
 
-    this.drawGraph(this.state.topTracks, param);
+    this.drawGraph(undefined, param);
     this.setState({ param });
-
-    let sortedTracks = this.sortTracks(this.state.topTracks, param);
-
-    // this.drawGraph()
-
-    // this.svgContainer.selectAll('.bar').sort((a,b) => {
-    //   return a[param] - b[param];
-    // })
-    // let x = d3.scaleBand()
-    //   .range([0, this.width])
-    //   .padding(0.1);
-    // let y = d3.scaleLinear()
-    //   .range([this.height, 0])
-    
-
-    // x.domain(sortedTracks.map(function(d){ return d.title }))
-
-    // y.domain([0, d3.max(sortedTracks, function(d) { return d[param] }) + 20])
-
-    // this.svgContainer.selectAll('.bar')
-    // .data(sortedTracks)
-
   }
-
-
-
 
   render(){
     return (
