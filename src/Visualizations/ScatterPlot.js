ebfd48673e3e024de616b8fa94c0d4d2502f56b8
dsdunn <dsdunnmusic@gmail.com>
2019-01-28 16:49:49 -0700

    Rotate major and minor labels

--- src/Visualizations/ScatterPlot.js
+++ src/Visualizations/ScatterPlot.js
@@ -15,7 +15,6 @@ class ScatterPlot extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    console.log('update')
     if (this.props.loading) {
       return;
     }
@@ -43,36 +42,32 @@ class ScatterPlot extends Component {
     let y = d3.scaleLinear()
       .range([this.height, 0]);
 
-    let colorScale = d3.scaleLinear()
-      .range(['#3944c7','#f12d0e','#881503'])
-      .domain([d3.min(tracks, function(d) {
-        return d.tempo;
-      }),
-      d3.max(tracks, function(d) {
-        return d.tempo
-      }) + 10]);
+    // let colorScale = d3.scaleLinear()
+    //   .range(['#3944c7','#f12d0e','#881503'])
+    //   .domain([d3.min(tracks, function(d) {
+    //     return d.tempo;
+    //   }),
+    //   d3.max(tracks, function(d) {
+    //     return d.tempo
+    //   }) + 10]);
 
-    let sizeScale = d3.scaleLinear()
-      .range([7, 20])
-      .domain([d3.min(tracks, function(d) {
-        return d.duration_ms;
-      }),
-      d3.max(tracks, function(d) {
-        return d.duration_ms;
-      })]);
-
-
-    // let plot = this.svgContainer.selectAll('g.node')
-    //   .data(tracks, d => { return d.title; });
+    // let sizeScale = d3.scaleLinear()
+    //   .range([7, 20])
+    //   .domain([d3.min(tracks, function(d) {
+    //     return d.duration_ms;
+    //   }),
+    //   d3.max(tracks, function(d) {
+    //     return d.duration_ms;
+    //   })]);
 
     const parseTime = d3.timeParse("%Y-%m-%d");
     const parseYear = d3.timeParse('%Y');
 
-    function keyMap(num) {
-      let keys = ['C','Db','D','Eb','E','F','F#','G','Ab','A','Bb','B'];
+    // function keyMap(num) {
+    //   let keys = ['C','Db','D','Eb','E','F','F#','G','Ab','A','Bb','B'];
 
-      return keys[num - 1];
-    }
+    //   return keys[num];
+    // }
 
     this.svgContainer.append('g')
       .attr('class', 'x2 axis')
@@ -80,20 +75,20 @@ class ScatterPlot extends Component {
     this.svgContainer.append('text')
       .attr('class', 'major-label')
       .text('Major Key')
-      .attr('transform', 'translate(-40,' + this.height / 10 + ')')
+      .attr('transform', 'translate(-40,' + this.height * .3 + ') rotate(270)')
     this.svgContainer.append('text')
       .attr('class', 'minor-label')
       .text('Minor Key')
-      .attr('transform', 'translate(-40,' + this.height * .9 + ')')
+      .attr('transform', 'translate(-40,' + this.height * .75 + ') rotate(270)')
     this.svgContainer.append('text')
       .attr('class', 'x-label')
-      .text('Release Date')
-      .attr('transform', 'translate(-40,' + this.height * .47 + ')')
+      .text('Release')
+      .attr('transform', 'translate(-60,' + this.height * .5 + ')')
 
     x.domain([
       d3.min(tracks, function(d) {
         return parseTime(d.releaseDate) || parseYear(d.releaseDate); 
-      }) - 94608000000, 
+      }), 
       new Date()
     ]);
 
@@ -101,49 +96,39 @@ class ScatterPlot extends Component {
 
     let simulation = d3.forceSimulation(tracks)
       .velocityDecay(0.7)
-      .force("x", d3.forceX(function(d) { return x(parseTime(d.releaseDate) || parseYear(d.releaseDate)); }).strength(1))
+      .force("x", d3.forceX(function(d) { return x(parseTime(d.releaseDate) || parseYear(d.releaseDate)) - 18; }).strength(1))
       .force("y", d3.forceY(y(0.5)))
       .force("y2", d3.forceY(function(d) { return y(d.mode); }))
-      .force("collide", d3.forceCollide().radius(function(d) { return 18 }).iterations(16))
+      .force("collide", d3.forceCollide().radius(function(d) { return 20 }))
       .on('tick', ticked);
 
+    let node = d3.select('.scatter')
+      .selectAll('.track')
+      .data(tracks, function(d) { return d.id })
 
-    // let defs = d3.select('.scatter')
-    //   .append('def')
+    node.exit()
+      .remove();
 
-    // tracks.forEach(function(d) {
-    //   defs.append('clip-path')
-    //   .attr('id', d.title)
-    //   .append('circle')
-    //     .attr('r', 18)
-    //     .attr('cx', d.x)
-    //     .attr('cy', d.y)
-    // })
-
-    function ticked() {
-
-      let node = d3.select('.scatter')
-        .selectAll('image')
-        .data(tracks, function(d) { return d.title })
-        
-      node.enter()
+    node = node.enter()
         .append('image')
-        .merge(node)
-        .attr('class', 'circle')
+        .attr('class', 'track')
         .attr('height', 36)
         .attr('width', 36)
-        .attr('x', function(d) {
-          return d.x
-        })
-        .attr('y', function(d) {
-          return d.y
-        })
         .attr('xlink:href', function(d) { return d.coverArt.url })
         .on('mouseover', function(d) {
           console.log(d)
         })
-        node.exit().remove();
-      }
+        .merge(node)
+
+
+    function ticked() {
+      node.attr('x', function(d) {
+        return d.x
+      })
+      .attr('y', function(d) {
+        return d.y
+      })
+    }
 
     d3.select('.x2')
       .call(d3.axisBottom().scale(x).tickFormat(d3.timeFormat("%Y")));
