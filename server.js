a6ff7a587525eb6cd67d7aed187bc1bffee505a7
dsdunn <dsdunnmusic@gmail.com>
2019-02-09 20:17:47 -0700

    Remove '/bar' from redirect

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get('/callback', (req, res) => {
   request.post(authOptions, (error, response, body) => {
     let access_token = body.access_token;
     let refresh_token = body.refresh_token;
-    let uri = (process.env.FRONTEND_URI || 'http://localhost:3000') + '/bar';
+    let uri = (process.env.FRONTEND_URI || 'http://localhost:3000') ;
 
 
     res.redirect(uri + '/#' +
