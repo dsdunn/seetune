8f3e8681710326d944a1e118e9540e1c18cb9298
dsdunn <dsdunnmusic@gmail.com>
2020-06-20 13:31:20 -0600

    Build

---
+++ client/build/precache-manifest.6f285e764f690dd09cc03ef03ae5d7fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa086827f87e95b17676",
+    "url": "/static/css/main.e148b435.chunk.css"
+  },
+  {
+    "revision": "aa086827f87e95b17676",
+    "url": "/static/js/main.aa086827.chunk.js"
+  },
+  {
+    "revision": "7022bb94e44e38bcec3c",
+    "url": "/static/js/1.7022bb94.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "11dc437ab8ef0c32289c092083d20068",
+    "url": "/static/media/logo_green.11dc437a.png"
+  },
+  {
+    "revision": "bd45dbdca630e99c0d3bfa358ef72a30",
+    "url": "/static/media/headphones_banner.bd45dbdc.jpg"
+  },
+  {
+    "revision": "ca250c448f2e839c5a62c364d186a33a",
+    "url": "/index.html"
+  }
+];
